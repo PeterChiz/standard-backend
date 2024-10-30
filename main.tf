a129cbc101c2d8fc1e9c2585e680908c7d5e9abf
PeterChiz <buithienchi209@gmail.com>
2024-10-30 14:42:46 +0700

    user1: add vm-user2

--- main.tf
+++ main.tf
@@ -16,3 +16,12 @@ resource "aws_volume_attachment" "ebs_attach" {
   volume_id = aws_ebs_volume.ebs_extend.id
   instance_id = aws_instance.vm-user1.id
 }
+
+resource "aws_instance" "vm-user2" {
+  ami = "ami-04b6019d38ea93034"
+  instance_type = "t3.micro"
+  availability_zone = "ap-southeast-1b"
+  tags = {
+    Name ="vm-user2"
+  }
+}
